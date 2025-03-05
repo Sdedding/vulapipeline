1461bf46c66b1faf17fcea057f63b87b8d09c5fc
Vula Author 1 <git@vula.link>
2025-03-05 22:38:52 +0100

    Use clang for highctidh install script

--- misc/install-highctidh.sh
+++ misc/install-highctidh.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
-apt install -y --no-install-recommends flit
+export CC=clang
+apt install -y --no-install-recommends flit clang
 git clone https://codeberg.org/vula/highctidh/
 cd highctidh;
 git checkout v1.0.2024092800
-make deb
+CC=$CC make deb
 ls -al *
 ls -al dist/
 dpkg -i `pwd`/dist/python3-highctidh*.deb
