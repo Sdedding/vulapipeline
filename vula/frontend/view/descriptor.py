c0cc48460804a2079ca537f956199b055c5ac232
Sander <sander@bfh.ch>
2025-06-16 22:03:08 +0200

    fasdf
    Signed-off-by: Sander <sander@bfh.ch>

--- vula/frontend/view/descriptor.py
+++ vula/frontend/view/descriptor.py
@@ -51,9 +51,10 @@ class DescriptorFrame(ttk.Notebook):
             ).items()
 
         }
-        print(descriptors)
+
 
         for ip, desc in descriptors.items():
+            ip = str(desc.v4a)
             ttk.Label(
                 text_frame,
                 text= ip,
